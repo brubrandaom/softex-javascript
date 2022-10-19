52a2421d0b48cd451f3c39fd0ecaebd4d41eb9a3
brubrandaom <85810656+brubrandaom@users.noreply.github.com>
2022-10-19 19:50:40 -0300

    Add files via upload

---
+++ softex-javascript/conexao/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const db = require('./models/db');
+
+app.get('/', async (req, res) => {
+    res.send("Página inicial");
+});
+
+conexao.authenticate()
+.then(()=>{
+    console.log("Conexão com banco de dados realizada com sucesso!");
+}).catch(()=>{
+    console.log("ERRO!");
+});
+
+app.listen(8080, () => {
+    console.log("Servidor iniciado na porta 8080: http://localhost:8080");
+});
